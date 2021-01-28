fac43d929c4f356ae209d063761c34987a11375f
Adib-Rezaei <adibrezaeish@gmail.com>
2021-01-28 20:03:38 +0330

    initial

---
+++ client/client.js
@@ -0,0 +1,63 @@
+console.log('Client');
+
+const form = document.querySelector('form');
+const loadingElement = document.querySelector('.loading');
+const tweetsElement = document.querySelector('.tweets');
+const API_URL = 'http://localhost:8000/tweet';
+
+loadingElement.style.display = 'none';
+
+listAlltweets();
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log('form submitted');
+    const formData = new FormData(form);
+    const name = formData.get('name');
+    const content = formData.get('content');
+
+    const tweet = { name, content};
+
+    form.style.display = 'none';
+    loadingElement.style.display = '';
+    fetch(API_URL, {
+        method: 'POST',
+        body: JSON.stringify(tweet),
+        headers: {
+            'content-type': 'application/json'
+        }
+    }).then(response => response.json())
+        .then(createdTweet => {
+            form.reset();
+            form.style.display = '';
+            listAlltweets();
+        });
+});
+
+function listAlltweets(){
+    tweetsElement.innerHTML = '';
+    fetch(API_URL)
+        .then(response => response.json())
+        .then(tweets => {
+            console.log(tweets);
+            tweets.reverse();
+            tweets.forEach(tweet => {
+                const div = document.createElement('div');
+
+                const header = document.createElement('h3');
+                header.textContent = tweet.name;
+
+                const contents = document.createElement('p');
+                contents.textContent = tweet.content;
+
+                const date = document.createElement('small');
+                date.textContent = new Date(tweet.created);
+
+                div.appendChild(header);
+                div.appendChild(contents);
+
+                tweetsElement.appendChild(div);
+            });
+            loadingElement.style.display = 'none';
+        });
+}
---
+++ server/index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const cors = require('cors');
+const monk = require('monk');
+require('dotenv/config');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const db = monk(process.env.DB_CONNECTION || 'localhost/meower');
+const tweets = db.get('tweets');
+const port = process.env.PORT || 8000;
+
+app.listen(8000, ()=> {
+    console.log(`listening on http://localhost:${port}`);
+});
+
+function isValidTweet(tweet){
+    return tweet.name && tweet.name.toString().trim() !== '' &&
+    tweet.content && tweet.content.toString().trim() !== '';
+}
+
+app.get('/tweet', (req, res) => {
+    tweets.find().then(tweets => {
+        tweets.reverse();
+        res.json(tweets);
+    });
+});
+
+app.post('/tweet', (req, res) => {
+    if (isValidTweet(req.body)){
+        const tweet_content = {
+            name: req.body.name.toString(),
+            content: req.body.content.toString(),
+            created: new Date()
+        };
+        
+        tweets.insert(tweet_content).then(createdTweet =>{
+            console.log("sth");
+            res.json(createdTweet);
+        });
+
+    } else {
+        res.status(422);
+        res.json({
+            message: "Name and content required"
+        });
+    }
+    console.log("DONE");
+});
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello world!'
+    })
+});
